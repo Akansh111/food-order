eae39450f9a87934882350377868d2d33cd38bde
Akansh Goel <akagoel2@publicisgroupe.net>
2020-04-24 22:49:33 +0530

    Updates

---
+++ precache-manifest.a55456e937edb6d8aba78f3de312d5f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230cec4464ec5b1fdde1cbca9e1b27b9",
+    "url": "/food-order/index.html"
+  },
+  {
+    "revision": "8b7ca554b24c48c4ed1d",
+    "url": "/food-order/static/css/2.2da405f0.chunk.css"
+  },
+  {
+    "revision": "ceafdee8ff10c89f567c",
+    "url": "/food-order/static/css/main.e5a191ac.chunk.css"
+  },
+  {
+    "revision": "8b7ca554b24c48c4ed1d",
+    "url": "/food-order/static/js/2.adb4e308.chunk.js"
+  },
+  {
+    "revision": "ac4b2c5a1c267dfd1d4681e8e7a64434",
+    "url": "/food-order/static/js/2.adb4e308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceafdee8ff10c89f567c",
+    "url": "/food-order/static/js/main.5956febd.chunk.js"
+  },
+  {
+    "revision": "6c52be67125823458479",
+    "url": "/food-order/static/js/runtime-main.32a73fc5.js"
+  },
+  {
+    "revision": "5cb4a63768796052affa987904eae19f",
+    "url": "/food-order/static/media/background2.5cb4a637.jpg"
+  }
+]);
